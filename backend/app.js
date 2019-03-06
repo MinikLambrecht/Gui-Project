7b9caea6d27d1d83338fa440ddade606365ae1d4
Minik Gaarde Lambrecht <MinikGLambrecht@hotmail.com>
2019-03-06 14:21:59 +0100

    Small dependency bug fixes and corrections

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const historyApiFallback = require('connect-history-api-fallback');
 const mongoose = require('mongoose');
+const path = require('path');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const config = require('./db');
 
-const users = require('./routes/user');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+
+const config = require('./db');
 const webpackConfig = require('./webpack.config');
 
+const isDev = process.env.NODE_ENV !== 'production';
+const PORT = process.env.PORT || 5000;
+
+const users = require('./routes/user');
+
+
+// Configuration
+// =================================================================================================
+
+// Setup mongoose
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
     () => {console.log('Database is connected') },
     err => { console.log('Can not connect to the database'+ err)}
@@ -19,14 +34,43 @@ require('./passport')(passport);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// API Routes
 app.use('/api/users', users);
 
-  const PORT = process.env.PORT || 5000;
+if (isDev){
+  const compiler = webpack(webpackConfig);
 
-  app.listen(PORT, (err) => {
-    if (err) {
-      console.log(err);
+  app.use(historyApiFallback({
+    verbose: false
+  }));
+
+  app.use(webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+    contentBase: path.resolve(__dirname, '../frontend/public'),
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false
     }
+  }));
 
-  console.log(`>>> 🌎 Open http://localhost:${PORT}/ in your browser.`);
+  app.use(webpackHotMiddleware(compiler));
+  app.use(express .static(path.resolve(__dirname, '../dist')));
+} else {
+  app.use(express.static(path.resolve(__dirname, '../dist')));
+  app.get('*', function(req, res) {
+    res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+    res.end();
+  });
+}
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  }
+
+ console.log(`>>> 🌎 Open http://localhost:${PORT}/ in your browser.`);
 });
